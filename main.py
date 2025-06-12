d4bb9befa88f9e67deb16a79cef3d07184ffadb9
leosteffani <108552354+leosteffani@users.noreply.github.com>
2025-06-12 15:02:16 -0300

    add p recursiva

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+função em sua linguagem preferida. A função recursiva deve receber
+um número N > 1 (validar o input), e retornar todos os
+números primos até o número N. EX. p(2) = [2]; p(3) = [2, 3]; p(10) = [2, 3, 5, 7];
+'''
+def p(numero):
+    if numero == 2:
+        return [2]
+    resposta = []
+    count = 0
+    for i in range(1,(numero//2)+1):
+        if (numero // i) == (numero / i):
+            count +=1
+    resposta = p(numero-1)
+    if count == 1:
+        resposta.append(numero)
+    return resposta
+print(p(10))
